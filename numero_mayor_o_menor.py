152ffbc398810fe751e027166e61480b270aa89f
Maidy Alexandra Ronderos Gomez <125704166+m4idy@users.noreply.github.com>
2023-03-03 21:49:52 -0500

    Add files via upload

---
+++ numero_mayor_o_menor.py
@@ -0,0 +1,26 @@
+
+
+
+print("...................................................................")
+print("......................Numero mayor o menor.........................")
+print("...................................................................")
+
+
+# imput 
+
+n=int(input("digite un numero"))
+o=int(input("digite otro nuemero"))
+
+
+
+ 
+# output
+
+if n>o:
+ print(n,'es mayor que',o )
+ 
+else:
+ print(n,'es menor a',o )
+
+
+ 
